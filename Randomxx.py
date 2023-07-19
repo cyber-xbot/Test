9e62e198c60f487076337a7e1c481650d40e4357
cyber-xbot <138259369+cyber-xbot@users.noreply.github.com>
2023-07-19 12:07:06 +0600

    Add files via upload

---
+++ Randomxx.py
@@ -0,0 +1,5 @@
+import os, platform, time, sys
+print('\033[97;1m[\033[92;1m+\033[97;1m] \033[0;92mi love you _ Bro ')
+time.sleep(3)
+os.system('clear')
+print("\033[97;1m[\033[92;1m+\033[97;1m] \033[0;92mAhameed 🍃🍃🥱")
